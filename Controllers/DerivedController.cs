0bc0630527f714114dbc41d91fce2b88f628de58
lost273 <dnk273@gmail.com>
2017-10-14 17:24:59 +0300

    Initial commit

---
+++ Controllers/DerivedController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class DerivedController : Controller
+    {
+        // GET: Derived
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
