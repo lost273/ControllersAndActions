39716729617d0de6ae4a107b1fd450e82d144606
lost273 <dnk273@gmail.com>
2017-10-15 20:48:30 +0300

    15-10-2017-20-48

--- Controllers/BasicController.cs
+++ Controllers/BasicController.cs
@@ -10,9 +10,13 @@ namespace ControllersAndActions.Controllers {
         public void Execute(RequestContext requestContext) {
             string controller = (string)requestContext.RouteData.Values["controller"];
             string action = (string)requestContext.RouteData.Values["action"];
-            requestContext.HttpContext.Response.Write(
+            if (action.ToLower() == "redirect") {
+                requestContext.HttpContext.Response.Redirect("/Derived/Index");
+            }
+            else {
+                requestContext.HttpContext.Response.Write(
                 string.Format("Controller: {0}, Action: {1}", controller, action));
-            
+            }    
         }
     }
 }
--- Controllers/DerivedController.cs
+++ Controllers/DerivedController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ControllersAndActions.Infrastructure;
 
 namespace ControllersAndActions.Controllers
 {
@@ -11,6 +12,15 @@ namespace ControllersAndActions.Controllers
         public ActionResult Index() {
             ViewBag.Message = "Hello from the DerivedController Index method";
             return View("MyView");
+        }
+        public ActionResult ProduceOutput() {
+            if (Server.MachineName == "TINY") {
+                return new CustomRedirectResult { Url = "/Basic/Index" };
+            }
+            else {
+                Response.Write("Controller: Derived, Action: ProduceOutput");
+                return null;
+            }
         }
     }
 }
